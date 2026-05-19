b62966a9a739219374bf87dd6c676ceb3818c0b9
HannesKolmer <hannes.kolmer.2007@gmail.com>
2026-05-19 14:59:33 +0200

    Initial Commit: add prime number checker

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int zahl;
+int counter = 0;
+
+Console.WriteLine("Gib die zu überprüfende Zahl ein: ");
+zahl = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= zahl; i++)
+{
+    if (zahl % i == 0)
+    {
+        counter++;
+    }
+}
+if (counter == 2)
+{
+    Console.WriteLine(zahl + " ist eine Primzahl");
+}
+else
+{
+    Console.WriteLine(zahl + " ist keine Primzahl");
+}
